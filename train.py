6f8d1bab22a770d57545cd2adb95bb2d2d42789b
Thong Luc <email_github_cua_ban@gmail.com>
2026-05-16 22:55:41 +0700

    update

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from tqdm import tqdm
+from tqdm.auto import tqdm
 import logging
 import datetime
 
